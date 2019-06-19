e6d824851c5f2aab1c7e0f7195240cbf7c7f418f
AnonMuk <zubair.mukhi@gmail.com>
2019-06-19 12:39:36 -0500

    Test and Revert

--- bulkindex.js
+++ bulkindex.js
@@ -2,11 +2,11 @@ const jwt_decode = require('jwt-decode');
 const readline = require('readline');
 const fs = require('fs');
 const rl = readline.createInterface({
-  input: fs.createReadStream('target.txt')
+  input: fs.createReadStream('target.txt'),
 });
   rl.on('line', function (line){
     var token = line;
     var decoded = jwt_decode(token);
-    console.log(jwt_decode(line));
+    console.log(decoded);
     rl.close();
 });
