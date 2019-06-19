c2bf48d411cb268a822c0f8c645d592dc58315f8
AnonMuk <zubair.mukhi@gmail.com>
2019-06-19 16:28:50 -0500

    Cleanup

--- processor.js
+++ processor.js
@@ -1,13 +1,15 @@
-var exports = module.exports = {};
 const jwt_decode = require('jwt-decode');
 const readline = require('readline');
 const fs = require('fs');
+var exports = module.exports;
 
+//single jwt processor
 exports.single = function oneJWT(token){
     var decoded = jwt_decode(token);
     console.log(decoded);
 }
 
+//bulk jwt processor from 'target.txt' in main directory
 exports.bulk = function bulkJWT(){
   const rl = readline.createInterface({
     input: fs.createReadStream('target.txt'),
