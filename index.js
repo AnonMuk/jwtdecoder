a6f4ff23434d7a8c8494327bf19af1f37c16f010
AnonMuk <zubair.mukhi@gmail.com>
2019-06-19 10:52:45 -0500

    Runs in command line now :D

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var jwt_decode = require('jwt-decode');
+const jwt_decode = require('jwt-decode');
+const readline = require('readline');
 
-var token = prompt("Enter token now");
-
-var decoded = jwt_decode(token);
-console.log(decoded);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+  rl.question('Enter a token, please: ', (token) => {
+    var decoded = jwt_decode(token);
+    console.log(decoded);
+    rl.close();
+});
